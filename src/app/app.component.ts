733c65b81af16085827e80b2905fa6ae73e9da9c
Grace Nyutu <graceynyutu@gmail.com>
2022-04-15 23:47:14 +0300

    create OnInit and OnDestroy lifecycles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
   title = 'gitsearch';
+  mySubscription: Subscription = new Subscription;
+
+  constructor (private apiService: ApiService){}
+
+  repos = []
+  ngOnInit(): void{
+  this.getPublicReposWithSubscription()
+ }
+  ngOnDestroy(): void {
+  this.mySubscription.unsubscribe();
+ }
+  getPublicReposWithSubscription(){
+  this.mySubscription.add(
+    this.apiService.getRepo("PulicRepo").subscription ((PulicRepo:any) => console.log (PulicRepo))
+  )
+ }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getRepo(username: string):any{
+    return this.http.get(`https://api.github.com/users/${username}/repos`)
+  }
 }
