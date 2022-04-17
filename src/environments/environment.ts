9a86d20beaa4f215ad77434a8d1df49f81f5a549
Grace Nyutu <graceynyutu@gmail.com>
2022-04-18 00:20:14 +0300

    place acess key in environment.ts file and place it in gitignore file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  accessKey:"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"
 };
 
 /*
