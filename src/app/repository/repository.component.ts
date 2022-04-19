c87aca4f45db4a35b21f59ded096ba2e4017aff2
Grace Nyutu <graceynyutu@gmail.com>
2022-04-19 07:07:17 +0300

    deploy

--- src/app/repository/repository.component.ts
+++ src/app/repository/repository.component.ts
@@ -9,6 +9,10 @@ import { ApiService } from '../services/api.service';
 export class RepositoryComponent implements OnInit {
   repos: any;
   repository: any;
+  http: any;
+
+  private clientid = 'ad64f1a34d8ef30c6bf2';
+  private clientsecret = 'cf54bcbbdf5160221d81bdc5bb3bb096d48980dc';
 
   constructor(private apiService: ApiService ) {
   }
@@ -21,6 +25,11 @@ export class RepositoryComponent implements OnInit {
     });
   }
   ngOnInit(): void {
+    this.getUserRepos()
+  }
+
+  getUserRepos(){
+    return this.http.get("https://api.github.com/users/" + this.repository + "?client_id=" + this.clientid + "?client_secret=" + this.clientsecret)
   }
 
 }
